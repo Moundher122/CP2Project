9a5cfd2e4f31214bcae8a7a8e5e510e5fd4b7cac
Wassim <sdk.wassim6@gmail.com>
2025-05-25 01:08:44 +0100

    nothing important

--- ProjectCore/asgi.py
+++ ProjectCore/asgi.py
@@ -4,27 +4,23 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
-import chat.routing  # Replace 'chat' with your app name if different
-#from whitenoise import ASGIStaticFilesWrapper
+import chat.routing  
 
-
-# Set up Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectCore.settings')
 django.setup()
 
 
-# Main ASGI application
+
 application = ProtocolTypeRouter({
-    # HTTP requests are handled by Django as usual
+    
     "http": get_asgi_application(),
 
-    # WebSocket requests go through the AuthMiddlewareStack,
-    # which adds `scope['user']` based on the session
+    
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns  #  Must be a list of `re_path`
+            chat.routing.websocket_urlpatterns 
         )
     ),
 })
 
-#application = ASGIStaticFilesWrapper(application)
+
